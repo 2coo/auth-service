ccc8a8992e1a4b2a997932cd3351a221d0df7ae1
Tuvshinbayar Tuvhshinzul <giva9712@gmail.com>
2020-10-29 17:15:50 +0800

    done

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,37 @@
-// import { PrismaClient, GrantType } from '@prisma/client'
+import { PrismaClient, GrantType } from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function seed() {
-//   const prisma = new PrismaClient()
-  
-//   prisma.oAuthGrant.create({
-//       data: {
-//           grantType: GrantType.AUTHORIZATION_CODE,
-
-//       }
-//   })
-//   await prisma.$disconnect()
+  const type1 = prisma.oAuthGrant.create({
+    data: {
+      grantType: GrantType.AUTHORIZATION_CODE,
+    },
+  })
+  const type2 = prisma.oAuthGrant.create({
+    data: {
+      grantType: GrantType.CLIENT_CREDENTIALS,
+    },
+  })
+  const type3 = prisma.oAuthGrant.create({
+    data: {
+      grantType: GrantType.EXTENSION,
+    },
+  })
+  const type4 = prisma.oAuthGrant.create({
+    data: {
+      grantType: GrantType.PASSWORD,
+    },
+  })
+  const type5 = prisma.oAuthGrant.create({
+    data: {
+      grantType: GrantType.REFRESH_TOKEN,
+    },
+  })
+  await prisma.$transaction([type1, type2, type3, type4, type5])
 }
 
 seed()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
